ac2ed0133817bb3a807316e831e8d3bd58cfbc54
Tânia Andrade <120664909+TaniaAndrade1984@users.noreply.github.com>
2023-06-19 02:01:04 -0300

    Finalizado

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+const botaoalterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocadeTema = document.querySelector(".imagem-botao")
+
+botaoalterarTema.addEventListener("click", () => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro");
+
+    if (modoEscuroEstaAtivo) {
+        imagemBotaoTrocadeTema.setAttribute("src", "./src/imagens/sun.png");
+    }
+    else {
+        imagemBotaoTrocadeTema.setAttribute("src", "./src/imagens/moon.png");
+
+    }
+
+});
